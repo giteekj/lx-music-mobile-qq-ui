c08fce470511cd4f9902124743746e72861d7a35
githubKj <47735855+giteekj@users.noreply.github.com>
2026-07-23 11:40:47 +0800

    feat: add QQ Music-inspired mobile UI

--- src/screens/Home/Views/Search/HeaderBar/SearchInput.tsx
+++ src/screens/Home/Views/Search/HeaderBar/SearchInput.tsx
@@ -54,7 +54,7 @@ export default forwardRef<SearchInputType, SearchInputProps>(({ onChangeText, on
   return (
     <Input
       ref={inputRef}
-      placeholder="Search for something..."
+      placeholder="搜索音乐、歌词..."
       value={text}
       onChangeText={handleChangeText}
       // style={{ ...styles.input, backgroundColor: theme['c-primary-input-background'] }}
