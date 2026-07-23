72a4750b22f036a1236664c38e1aaa44659d5032
githubKj <47735855+giteekj@users.noreply.github.com>
2026-07-23 11:40:31 +0800

    feat: add QQ Music-inspired mobile UI

--- src/components/player/PlayerBar/components/Title.tsx
+++ src/components/player/PlayerBar/components/Title.tsx
@@ -1,7 +1,6 @@
 import { TouchableOpacity } from 'react-native'
 import { navigations } from '@/navigation'
 import { usePlayerMusicInfo } from '@/store/player/hook'
-// import { toast } from '@/utils/tools'
 import { useSettingValue } from '@/store/setting/hook'
 import { useTheme } from '@/store/theme/hook'
 import commonState from '@/store/common/state'
@@ -12,17 +11,13 @@ import { createStyle, formatMusicName } from '@/utils/tools'
 
 
 export default ({ isHome }: { isHome: boolean }) => {
-  // const { t } = useTranslation()
   const musicInfo = usePlayerMusicInfo()
   const downloadFileName = useSettingValue('download.fileName')
   const theme = useTheme()
 
   const handlePress = () => {
-    // console.log('')
-    // console.log(playMusicInfo)
     if (!musicInfo.id) return
     navigations.pushPlayDetailScreen(commonState.componentIds.home!)
-    // toast(global.i18n.t('play_detail_todo_tip'), 'long')
   }
 
   const handleLongPress = () => {
@@ -30,47 +25,23 @@ export default ({ isHome }: { isHome: boolean }) => {
     if (!listId || listId == LIST_IDS.DOWNLOAD) return
     global.app_event.jumpListPosition()
   }
-  // console.log('render title')
 
   const title = musicInfo.id
     ? musicInfo.singer
       ? formatMusicName(downloadFileName, musicInfo.name, musicInfo.singer)
       : musicInfo.name
-    : ''
-  // console.log(playMusicInfo)
+    : 'LX Music'
+
   return (
     <TouchableOpacity style={styles.container} onLongPress={handleLongPress} onPress={handlePress} activeOpacity={0.7} >
-      <Text color={theme['c-font-label']} numberOfLines={1}>{title}</Text>
+      <Text color={theme['c-font']} numberOfLines={1} size={14}>{title}</Text>
     </TouchableOpacity>
   )
 }
-// const Singer = () => {
-//   const playMusicInfo = useGetter('player', 'playMusicInfo')
-//   return (
-//     <View style={{ flexGrow: 0, flexShrink: 0 }}>
-//       <Text style={{ width: '100%', color: AppColors.normal }} numberOfLines={1}>
-//         {playMusicInfo ? playMusicInfo.musicInfo.singer : ''}
-//       </Text>
-//     </View>
-//   )
-// }
-// const MusicName = () => {
-//   const playMusicInfo = useGetter('player', 'playMusicInfo')
-//   return (
-//     <View style={{ flexGrow: 0, flexShrink: 1 }}>
-//       <Text style={{ width: '100%', color: AppColors.normal }} numberOfLines={1}>
-//         {playMusicInfo ? playMusicInfo.musicInfo.name : '^-^'}
-//       </Text>
-//     </View>
-//   )
-// }
 
 const styles = createStyle({
   container: {
     width: '100%',
     paddingHorizontal: 2,
-    // paddingBottom: 4,
-    // height: '50%',
-    // backgroundColor: 'rgba(0, 0, 0, .1)',
   },
 })
