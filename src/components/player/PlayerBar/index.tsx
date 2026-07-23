98a3667a60652e2fddc0040cdf28bfa63c49795f
githubKj <47735855+giteekj@users.noreply.github.com>
2026-07-23 11:40:34 +0800

    feat: add QQ Music-inspired mobile UI

--- src/components/player/PlayerBar/index.tsx
+++ src/components/player/PlayerBar/index.tsx
@@ -1,5 +1,5 @@
 import { memo, useMemo } from 'react'
-import { View } from 'react-native'
+import { View, TouchableOpacity, StyleSheet } from 'react-native'
 import { useKeyboard } from '@/utils/hooks'
 
 import Pic from './components/Pic'
@@ -7,83 +7,73 @@ import Title from './components/Title'
 import PlayInfo from './components/PlayInfo'
 import ControlBtn from './components/ControlBtn'
 import { createStyle } from '@/utils/tools'
-// import { useSettingValue } from '@/store/setting/hook'
 import { useTheme } from '@/store/theme/hook'
 import { useSettingValue } from '@/store/setting/hook'
+import { scaleSizeW } from '@/utils/pixelRatio'
+import { useIsPlay } from '@/store/player/hook'
+import { togglePlay } from '@/core/player/player'
+import { Icon } from '@/components/common/Icon'
 
 
 export default memo(({ isHome = false }: { isHome?: boolean }) => {
-  // const { onLayout, ...layout } = useLayout()
   const { keyboardShown } = useKeyboard()
   const theme = useTheme()
   const autoHidePlayBar = useSettingValue('common.autoHidePlayBar')
 
   const playerComponent = useMemo(() => (
-    <View style={{ ...styles.container, backgroundColor: theme['c-content-background'] }}>
-      <Pic isHome={isHome} />
-      <View style={styles.center}>
-        <Title isHome={isHome} />
-        {/* <View style={{ ...styles.row, justifyContent: 'space-between' }}>
-          <PlayTime />
-        </View> */}
-        <PlayInfo isHome={isHome} />
-      </View>
-      <View style={styles.right}>
-        <ControlBtn />
+    <View style={{ ...styles.container, backgroundColor: theme['c-main-background'] }}>
+      {/* 顶部细线 */}
+      <View style={[styles.topLine, { backgroundColor: theme['c-primary-alpha-200'] }]} />
+      <View style={styles.content}>
+        <Pic isHome={isHome} />
+        <TouchableOpacity style={styles.center} activeOpacity={0.7} onPress={togglePlay}>
+          <Title isHome={isHome} />
+          <PlayInfo isHome={isHome} />
+        </TouchableOpacity>
+        <View style={styles.right}>
+          <ControlBtn />
+        </View>
       </View>
     </View>
   ), [theme, isHome])
 
-  // console.log('render pb')
-
   return autoHidePlayBar && keyboardShown ? null : playerComponent
 })
-
 
 const styles = createStyle({
   container: {
     width: '100%',
-    // height: 100,
-    // paddingTop: progressContentPadding,
-    // marginTop: -progressContentPadding,
-    // backgroundColor: 'rgba(0, 0, 0, .1)',
-    // borderTopWidth: BorderWidths.normal2,
-    paddingVertical: 5,
-    paddingLeft: 5,
-    // backgroundColor: AppColors.primary,
-    // backgroundColor: 'red',
-    borderTopLeftRadius: 6,
-    borderTopRightRadius: 6,
+    paddingHorizontal: scaleSizeW(8),
+    paddingVertical: scaleSizeW(6),
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: -2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 8,
+  },
+  topLine: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: StyleSheet.hairlineWidth,
+  },
+  content: {
     flexDirection: 'row',
     alignItems: 'center',
-    elevation: 10,
-  },
-  left: {
-    // borderRadius: 3,
-    flexGrow: 0,
-    flexShrink: 0,
   },
   center: {
     flexDirection: 'column',
     flexGrow: 1,
     flexShrink: 1,
-    paddingLeft: 5,
-    height: '100%',
-    // justifyContent: 'space-evenly',
-    // height: 48,
-    // backgroundColor: 'rgba(0, 0, 0, .1)',
+    paddingLeft: scaleSizeW(10),
+    paddingRight: scaleSizeW(8),
+    justifyContent: 'center',
   },
   right: {
     flexDirection: 'row',
     alignItems: 'center',
     flexGrow: 0,
     flexShrink: 0,
-    paddingLeft: 5,
-    paddingRight: 5,
   },
-  // row: {
-  //   flexDirection: 'row',
-  //   flexGrow: 0,
-  //   flexShrink: 0,
-  // },
 })
